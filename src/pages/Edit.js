c9aa161235a5884c8cc3fd92248afa2b39b5f454
jeeweb <52396673+jeeweb@users.noreply.github.com>
2023-04-08 11:19:32 +0900

    useParams, useSearchParams, useNavigate

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,8 +1,36 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+  const navigate = useNavigate();
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const id = searchParams.get("id");
+  console.log("id: ", id);
+
+  const mode = searchParams.get("mode");
+  console.log("mode: ", mode);
+
   return (
     <div>
       <h1>Edit</h1>
       <p>이곳은 일기 수정페이지 입니다.</p>
+      <button onClick={() => setSearchParams({ who: "홍길동" })}>
+        QS바꾸기
+      </button>
+      <button
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        HOME으로 가기
+      </button>
+      <button
+        onClick={() => {
+          navigate(-1);
+        }}
+      >
+        뒤로가기
+      </button>
     </div>
   );
 };
