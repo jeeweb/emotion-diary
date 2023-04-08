54db862423f3b9d32f81ecc0f1805d6ca9108246
jeeweb <52396673+jeeweb@users.noreply.github.com>
2023-04-08 12:46:32 +0900

    공통 컴포넌트 작업 - button, header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import RouteTest from "./components/RouteTest";
-
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Diary from "./pages/Diary";
 
+// COMPONENTS
+import MyButton from "./components/MyButton";
+import MyHeader from "./components/MyHeader";
+
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <MyHeader
+          headText={"APP"}
+          leftChild={
+            <MyButton text={"왼쪽버튼"} onClick={() => alert("왼쪽클릭")} />
+          }
+          rightChild={
+            <MyButton text={"오른쪽버튼"} onClick={() => alert("오른쪽클릭")} />
+          }
+        />
         <h2>App.js</h2>
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+          type={"positive"}
+        />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />
           <Route path="/edit" element={<Edit />} />
           <Route path="/diary/:id" element={<Diary />} />
         </Routes>
-        <RouteTest />
       </div>
     </BrowserRouter>
   );
