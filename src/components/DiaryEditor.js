78ae3663a3c147201f32df768572a57ba480f3c2
jeeweb <52396673+jeeweb@users.noreply.github.com>
2023-04-27 23:59:46 +0900

    일기 상세페이지 구현

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -6,42 +6,13 @@ import MyHeader from "./../components/MyHeader";
 import MyButton from "./../components/MyButton";
 import EmotionItem from "./EmotionItem";
 
+import { getStringDate } from "../util/date";
+import { emotionList } from "../util/emotion";
+
 /* 이미지가 안뜨는 경우
   const env = process.env;
   env.PUBLIC_URL = env.PUBLIC_URL || "";
 */
-
-const emotionList = [
-  {
-    emotion_id: 1,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion1.png`,
-    emotion_descript: "완전 좋음",
-  },
-  {
-    emotion_id: 2,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion2.png`,
-    emotion_descript: "좋음",
-  },
-  {
-    emotion_id: 3,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion3.png`,
-    emotion_descript: "그럭저럭",
-  },
-  {
-    emotion_id: 4,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion4.png`,
-    emotion_descript: "나쁨",
-  },
-  {
-    emotion_id: 5,
-    emotion_img: process.env.PUBLIC_URL + `/assets/emotion5.png`,
-    emotion_descript: "끔찍함",
-  },
-];
-
-const getStringDate = (date) => {
-  return date.toISOString().slice(0, 10);
-};
 
 const DiaryEditor = ({ isEdit, originData }) => {
   const [date, setDate] = useState(getStringDate(new Date()));
